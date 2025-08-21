37b4273e674860586341a383b59dc117da61f788
Maryna32 <marinaburda053@gmail.com>
2025-08-21 13:35:11 +0300

    fixing warning

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher(['/', '/products(.*)', '/about']);
 
-export default clerkMiddleware(async (auth, req) => {
-  const session = await auth();
-
-  if (!isPublicRoute(req) && !session.userId) {
-    return session.redirectToSignIn();
-  }
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) auth().protect();
 });
 
 export const config = {

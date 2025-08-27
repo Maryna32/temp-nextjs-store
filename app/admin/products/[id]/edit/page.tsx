a8c82063e8cbb153bcddec947fc618aea416a7c0
Maryna32 <marinaburda053@gmail.com>
2025-08-27 16:48:02 +0300

    trying fixing buh

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,4 +1,4 @@
-import { fetchAdminProductDetails, updateProductAction,  updateProductImageAction} from '@/utils/actions';
+import { fetchAdminProductDetails, updateProductAction, updateProductImageAction } from '@/utils/actions';
 import FormContainer from '@/components/form/FormContainer';
 import FormInput from '@/components/form/FormInput';
 import PriceInput from '@/components/form/PriceInput';
@@ -7,57 +7,62 @@ import { SubmitButton } from '@/components/form/Buttons';
 import CheckBoxInput from '@/components/form/CheckBoxInput';
 import ImageInputContainer from '@/components/form/ImageInputContainer';
 
-async function EditProductPage({ params }: { params: { id: string } }) {
+interface EditProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function EditProductPage({ params }: EditProductPageProps) {
   const { id } = params;
   const product = await fetchAdminProductDetails(id);
   const { name, company, description, featured, price } = product;
+
   return (
     <section>
-      <h1 className='text-2xl font-semibold mb-8 capitalize'>update product</h1>
-      <div className='border p-8 rounded-md'>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">update product</h1>
+      <div className="border p-8 rounded-md">
         <ImageInputContainer
-      action={updateProductImageAction}
-      name={name}
-      image={product.image}
-      text='update image'
-    >
-      <input type='hidden' name='id' value={id} />
-      <input type='hidden' name='url' value={product.image} />
-    </ImageInputContainer>
+          action={updateProductImageAction}
+          name={name}
+          image={product.image}
+          text="update image"
+        >
+          <input type="hidden" name="id" value={id} />
+          <input type="hidden" name="url" value={product.image} />
+        </ImageInputContainer>
         <FormContainer action={updateProductAction}>
-          <div className='grid gap-4 md:grid-cols-2 my-4'>
-            <input type='hidden' name='id' value={id} />
+          <div className="grid gap-4 md:grid-cols-2 my-4">
+            <input type="hidden" name="id" value={id} />
             <FormInput
-              type='text'
-              name='name'
-              label='product name'
+              type="text"
+              name="name"
+              label="product name"
               defaultValue={name}
             />
             <FormInput
-              type='text'
-              name='company'
-              label='company'
+              type="text"
+              name="company"
+              label="company"
               defaultValue={company}
             />
-
             <PriceInput defaultValue={price} />
           </div>
           <TextAreaInput
-            name='description'
-            labelText='product description'
+            name="description"
+            labelText="product description"
             defaultValue={description}
           />
-          <div className='mt-6'>
+          <div className="mt-6">
             <CheckBoxInput
-              name='featured'
-              label='featured'
+              name="featured"
+              label="featured"
               defaultChecked={featured}
             />
           </div>
-          <SubmitButton text='update product' className='mt-8' />
+          <SubmitButton text="update product" className="mt-8" />
         </FormContainer>
       </div>
     </section>
   );
 }
-export default EditProductPage;
